fca336f90821b86e0f2b2ae105ece5752252e3b9
the-gdmo <pframm3@gmail.com>
2026-09-02 15:04:39 -0500

    change cleanup job cron to register at midnight UTC instead of 23:00 UTC

--- src/blocks/config/constants.ts
+++ src/blocks/config/constants.ts
@@ -6,6 +6,6 @@ export const ADHOC_CLEANUP_JOB = "cleanupDeletedAccountsAdhoc";
 // export const UPGRADE_NOTIFIER_JOB = "upgradeNotifier";
 
 // Job Cron
-export const CLEANUP_JOB_CRON = "0 23 * * *";
+export const CLEANUP_JOB_CRON = "0 0 * * *";
 export const MODINFO_CRON = "* * * * *";
 // export const UPGRADE_NOTIFIER_CRON = "* * * * *";
